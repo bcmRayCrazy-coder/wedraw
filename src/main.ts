ccaa4b5e339f07eab98f0c8850e42e738278ef73
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2022-09-13 21:49:19 +0800

    Launch menu complete!

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
-export default async function(){
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+
+import LaunchMenu from './menu/LaunchMenu';
+
+export default async function () {
     // Main program entry
+    const launchMenu = new LaunchMenu();
+    var option = (
+        await inquirer.prompt({
+            name: 'menu',
+            type: 'list',
+
+            message: '----Menu----',
+            choices: launchMenu.getTexts(),
+        })
+    ).menu;
+    var state: boolean = await launchMenu.run(option);
+    if (!state) console.log(chalk.bold.red('Ops, something error!'));
+    else console.log(chalk.bold.green('Done!'));
 }
